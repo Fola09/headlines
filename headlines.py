c3e85af29b89c91257423fabb5a192a0a3e00fad
fola09 <folaolayiwola.fo@gmail.com>
2020-04-03 00:38:52 +0100

    Post requests

--- headlines.py
+++ headlines.py
@@ -1,5 +1,5 @@
 import feedparser
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,11 +9,14 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 
-@app.route('/')
-@app.route('/<publication>')
-def get_news(publication='bbc'):
+@app.route('/', methods=['GET', 'POST'])
+def get_news():
+    query = request.form.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
     return render_template('home.html', articles=feed['entries'])
 
 
